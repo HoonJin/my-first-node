c013e2706126b7cdc067667c6c3072c4e239c705
Daniel Ji <bwjhj1030@gmail.com>
2021-05-15 01:25:44 +0900

    route를 조금 더 예쁘게 만들어보기

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -1,13 +1,19 @@
 import { Router } from 'express'
+import UAParser from 'ua-parser-js'
 
 const testRouter = Router()
 
-testRouter.route('/')
-  .get(async (req, res) => {
-    res.json({test: 'get'})
+testRouter
+  .use((req, res, next) => {
+    req.ua = new UAParser(req.header('user-agent'))
+    next()
   })
-  .post(async (req, res) => {
-    res.json({test: 'post'})
-  })
+  .route('/')
+    .get(async (req, res) => {
+      res.json(req.ua.getResult())
+    })
+    .post(async (req, res) => {
+      res.json({test: 'post'})
+    })
 
 export default testRouter
